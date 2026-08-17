5e283e267c3c2956c6ee448240b55af7b276a65f
Dino Krtanjek <krtanjekdino@gmail.com>
2026-08-17 22:04:57 +0200

    Automatic external monitor hypr config

---
+++ .config/hypr/common/setup-monitors.lua
@@ -0,0 +1,72 @@
+local function setupMonitors(withNotif)
+    local monitors = hl.get_monitors()
+
+    local auxW = 0
+    local auxH = 0
+    local laptopW = 0
+    local laptopH = 0
+
+    for _, monitor in ipairs(monitors) do
+        if monitor.name == "HDMI-A-5" then
+            auxW = monitor.width
+            auxH = monitor.height
+        elseif monitor.name == "eDP-1" then
+            laptopW = monitor.width
+            laptopH = monitor.height
+        end
+    end
+
+    if auxW > 0 then
+        hl.monitor({
+            output = "eDP-1",
+            mode = "preferred",
+            position = auxW .. "x" .. (auxH - laptopH),
+            scale = 1,
+            vrr = 0
+        })
+
+        hl.monitor({
+            output = "HDMI-A-5",
+            mode = "preferred",
+            position = "0x0",
+            scale = auxW > 2560 and 1.5 or 1,
+            vrr = 0
+        })
+
+        -- hl.dsp.moveworkspacetomonitor({
+        --     workspace = 1,
+        --     monitor = "HDMI-A-5",
+        -- })
+
+        hl.dsp.focus({
+            monitor = "HDMI-A-5",
+        })
+
+        if withNotif then
+            hl.notification.create({ text = "External monitor detected", timeout = 10000 })
+        end
+    else
+        -- hl.dsp.moveworkspacetomonitor({
+        --     workspace = 1,
+        --     monitor = "eDP-1",
+        -- })
+
+        hl.dsp.focus({
+            monitor = "eDP-1",
+        })
+
+        if withNotif then
+            hl.notification.create({ text = "External monitor disconnected", timeout = 10000 })
+        end
+    end
+end
+
+local notifyAfter = os.time() + 5
+local handleEvent = function()
+    setupMonitors(notifyAfter < os.time())
+end
+
+hl.on("monitor.added", handleEvent)
+hl.on("monitor.removed", handleEvent)
+
+setupMonitors(false)
--- .config/hypr/standalone/monitor.lua
+++ .config/hypr/standalone/monitor.lua
@@ -1,3 +1,5 @@
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto", vrr = 0 })
-hl.monitor({ output = "eDP-1", mode = "1920x1080@60.008", position = "2560x360", scale = 1, vrr = 0 })
-hl.monitor({ output = "desc:Dell Inc. DELL U2715H GH85D84G0KXS", mode = "2560x1440@59.951", position = "0x0", scale = 1, vrr = 0 })
+hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = 1, vrr = 0 })
+hl.monitor({ output = "HDMI-A-5", mode = "preferred", position = "0x0", scale = "auto", vrr = 0 })
+
+require("common.setup-monitors")
