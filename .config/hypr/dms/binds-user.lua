1bc843300b7c2d0bdf2afa0094b904d292099b2b
Dino Krtanjek <krtanjekdino@gmail.com>
2026-08-17 02:40:17 +0200

    Tracking custom dms binds

---
+++ .config/hypr/dms/binds-user.lua
@@ -0,0 +1,4 @@
+-- DMS user keybind overrides (edit via Control Center or dms; do not remove this header)
+
+hl.unbind("SUPER + T")
+hl.bind("SUPER + T", hl.dsp.exec_cmd("ghostty"), { description = "ghostty" })
