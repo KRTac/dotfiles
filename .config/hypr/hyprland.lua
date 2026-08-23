ddc8a18ed0d6ee42678dfeb84542de438ed387c5
Dino Krtanjek <krtanjekdino@gmail.com>
2026-08-23 13:30:27 +0200

    Hyprland importing hyprmod file if present

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -1,1 +1,12 @@
+function file_exists(name)
+  local f=io.open(name, "r")
+  if f~=nil then io.close(f) return true else return false end
+end
+
+
 require("dms.init")
+
+local hyprmodImport = "hyprland-gui"
+if file_exists(os.getenv("HOME") .. "/.config/hypr/" .. hyprmodImport .. ".lua") then
+  require(hyprmodImport)
+end
